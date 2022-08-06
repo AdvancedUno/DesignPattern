9f73ee18560065605bcce1eaff7f30badec7fb0c
AdvancedUno <ehlee8276@gmail.com>
2022-08-06 19:46:58 +0900

    MVVM

---
+++ MVVM/MultipleMVVM/Models/UnoModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MultipleMVVM.Models
+{
+    public partial class UnoModel
+    {
+        private string name;
+
+        private BitmapImage runImage;
+
+        private BitmapImage jumpImage;
+
+        private BitmapImage standImage;
+
+        private string commandAction;
+
+        public UnoModel(String name)
+        {
+            this.Name = name;
+        }
+        private void InitImage()
+        {
+            if(name != "Uno" && name != "NoUno") { return; }
+            CreateBitmap(ref runImage, "C:/Users/sales/Desktop/tempImage/"+name +"/run.png");
+            CreateBitmap(ref jumpImage, "C:/Users/sales/Desktop/tempImage/" + name + "/jump.png");
+            CreateBitmap(ref standImage, "C:/Users/sales/Desktop/tempImage/" + name + "/stand.png");
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { 
+                name = value;
+                InitImage();
+            }
+        }
+
+        public BitmapImage RunImage
+        {
+            get
+            {
+                return runImage;
+            }
+            set
+            {
+                runImage = value;
+            }
+        }
+
+        public BitmapImage JumpImage
+        {
+            get
+            {
+                return jumpImage;
+            }
+            set
+            {
+                jumpImage = value;
+
+            }
+        }
+        public BitmapImage StandImage
+        {
+            get
+            {
+                return standImage;
+            }
+            set
+            {
+                standImage = value;
+            }
+        }
+
+        public string CommandAction
+        {
+            get
+            {
+                return commandAction;
+            }
+            set
+            {
+                commandAction = value;
+            }
+        }
+        private void CreateBitmap(ref BitmapImage img, string filePath)
+        {
+            img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri(filePath, UriKind.Absolute);
+            img.EndInit();
+        }
+    }
+}
---
+++ MVVM/UnoMvvmSample/ViewModels/ViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UnoMvvmSample.Models;
+
+namespace UnoMvvmSample.ViewModels
+{
+    public partial class ViewModel : INotifyPropertyChanged
+	{
+        private Model hello;
+
+		private String outTxt;
+        public ViewModel()
+        {
+            hello = new Model();
+        }
+
+        public String OutTxt
+        {
+            get
+            {
+                return outTxt;
+            }
+            set
+            {
+				outTxt = value;
+				OnPropertyChanged("OutTxt");
+			}
+        }
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+
+		private ICommand showCommand;
+        public ICommand ShowCommnad
+        {
+            get { return (this.showCommand) ?? (this.showCommand = new DelegateCommand(Show)); }
+        }
+
+        private void Show()
+        {
+
+			OutTxt = hello.OutputTxt;
+
+
+		}
+
+    }
+
+	public class DelegateCommand : ICommand
+	{
+		private readonly Func<bool> canExecute;
+		private readonly Action execute;
+
+		public DelegateCommand(Action exectue) : this(exectue, null)
+		{
+		}
+
+		public DelegateCommand(Action execute, Func<bool> canExecute)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+		}
+
+		public event EventHandler CanExecuteChanged;
+		public bool CanExecute(object parameter)
+		{
+			if (this.canExecute == null)
+			{
+				return true;
+			}
+			return this.canExecute();
+		}
+
+		public void Execute(object parameter)
+		{
+			this.execute();
+		}
+
+		public void RaiseCanExecuteChanged()
+		{
+			if (this.CanExecuteChanged != null)
+			{
+				this.CanExecuteChanged(this, EventArgs.Empty);
+			}
+		}
+
+	}
+}
